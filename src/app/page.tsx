9bbeb53b6314a8438b5a6f5a17c8958ec8786222
dhanujdev <dhanujg499@gmail.com>
2025-03-15 14:18:09 -0400

    Remove Testimonials and OurPromise sections, update store location to single Hyderabad store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import HeroSection from "@/components/home/HeroSection";
 import CategorySection from "@/components/home/CategorySection";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
-import OurPromise from "@/components/home/OurPromise";
-import Testimonials from "@/components/home/Testimonials";
 import StoreLocations from "@/components/home/StoreLocations";
 import Newsletter from "@/components/home/Newsletter";
 
@@ -12,8 +10,6 @@ export default function Home() {
       <HeroSection />
       <CategorySection />
       <FeaturedProducts />
-      <OurPromise />
-      <Testimonials />
       <StoreLocations />
       <Newsletter />
     </>
